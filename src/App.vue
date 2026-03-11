6539545079d7ccdc2aac01c272fc92d28744f860
iVerdugoDev <ivan270@gmail.com>
2026-03-11 11:45:26 -0300

    project init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1> Nuevo Componente</h1>
+</template>
+
+<script>
+export default {
+  name: 'component-name',
+  // props: {},
+  data: function () {
+    return {
+      usuarios: [
+        { nombre: 'Ana', edad: 28 },
+        { nombre: 'Luis', edad: 34 },
+        { nombre: 'María', edad: 22 },
+        { nombre: 'Carlos', edad: 45 }
+      ]
+    }
+  },
+  // computed: {},
+  //methods: {},
+  // watch: {},
+  // components: {},
+  // mixins: [],
+  // filters: {},
+  // -- Lifecycle Methods
+  // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped></style>
